9c95119992437aa8f8fefba9fc1c368347d015d5
nekagit <nenad99@hotmail.de>
2025-04-23 09:36:30 +0200

    Add: constants, utils, helpers

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+
+class TImages {
+
+  static const String darkAppLogo = 'assets/logos/dark-logo.png';
+  static const String lightAppLogo = 'assets/logos/white-logo.png';
+
+  static const String google = 'assets/logos/google-logo.png';
+  static const String facebook = 'assets/logos/facebook-logo.png';
+}
---
+++ lib/utils/helpers/helper_functions.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class THelperFunctions {
+  static final Map<String, Color> _colorMap = {
+    'Red': Colors.red,
+    'Green': Colors.green,
+    'Blue': Colors.blue,
+    'Pink': Colors.pink,
+    'Grey': Colors.grey,
+    'Purple': Colors.purple,
+    'Black': Colors.black,
+    'White': Colors.white,
+    'Yellow': Colors.yellow,
+    'Orange': Colors.orange,
+    'Brown': Colors.brown,
+    'Cyan': Colors.cyan,
+    'Indigo': Colors.indigo,
+    'Lime': Colors.lime,
+    'Teal': Colors.teal,
+    'Amber': Colors.amber,
+    'DeepOrange': Colors.deepOrange,
+    'DeepPurple': Colors.deepPurple,
+    'LightBlue': Colors.lightBlue,
+    'LightGreen': Colors.lightGreen,
+    'BlueGrey': Colors.blueGrey,
+  };
+
+  static Color? getColor(String value) {
+    // Normalize input to ensure case-insensitivity
+    return _colorMap[value.trim().replaceAll(' ', '').capitalizeFirst];
+  }
+
+  static void showSnackBar(String message) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  static void showAlert(String title, String message) {
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            )
+          ]
+        );
+      }
+    );
+  }
+
+  static void navigateToScreen(BuildContext context, Widget screen) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => screen)
+    );
+  }
+
+  static String truncateText(String text, int maxLength) {
+    if (text.length <= maxLength) {
+      return text;
+    }else {
+      return '${text.substring(0, maxLength)}...';
+    }
+  }
+
+  static bool isDarkMode(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize() {
+    return MediaQuery.of(Get.context!).size;
+  }
+
+  static double screenHeight() {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static double screenWidth() {
+    return MediaQuery.of(Get.context!).size.width;
+  }
+
+  static String getFormattedDate(DateTime date, {String format = 'dd MMM yyyy'}) {
+    return DateFormat(format).format(date);
+  }
+
+  static List<T> removeDuplicates<T>(List<T> list) {
+    return list.toSet().toList();
+  }
+
+  static List<Widget> wrapWidgets(List<Widget> widgets, int rowSize){
+    final wrappedList = <Widget> [];
+    for (var i = 0; i< widgets.length; i += rowSize){
+      final rowChildren = widgets.sublist(i,i + rowSize > widgets.length ? widgets.length : i + rowSize);
+      wrappedList.add(Row(children: rowChildren));
+    }
+    return wrappedList;
+  }
+}
